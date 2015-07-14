6dbd1d8993fefd976fb82e15fcee4bcf8072c4d9
Nick Ostrovsky <nick@firedev.com>
2015-07-14 12:13:08 +0700

    cache bust

---
+++ bust.js
@@ -0,0 +1,14 @@
+var bust = require('cache-bust');
+var path = require('path');
+var file = path.resolve(__dirname, 'build/bundle.js');
+
+var hash = bust.alias(file).split('/');
+bust(file);
+
+var replace = require('replace-in-file');
+
+replace({
+  files: 'index.html',
+  replace: /bundle.*\.js/g,
+  with: hash[hash.length - 1]
+});
