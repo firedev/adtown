bd0c50f69b99c7a7434181081dcc1c665515000a
Nick Ostrovsky <nick@firedev.com>
2015-07-14 10:40:50 +0700

    Owned counter

--- app/components/HomeScreen.jsx
+++ app/components/HomeScreen.jsx
@@ -2,9 +2,31 @@ import React from 'react';
 import Buildings from './Buildings';
 
 var HomeScreen = React.createClass({
+  countOwned: function() {
+    var owned = 0;
+    this.props.buildings.forEach(function(building) {
+      if(building.owned) {
+        owned++;
+      }
+    });
+    return owned;
+  },
+
+  renderOwnedCount: function() {
+    var owned = this.countOwned();
+    if(owned > 0) {
+      return (
+        <div className="absolute top-0 left-0 mt2 ml2 bg-white rounded p1 z1">
+          Owned: <b>{owned}</b>
+        </div>
+      );
+    }
+  },
+
   render: function() {
     return <div className="holder">
       <div className="sky">
+        {this.renderOwnedCount()}
         <Buildings
           buildings={this.props.buildings}
           onClick={this.props.onBuildingClick}
